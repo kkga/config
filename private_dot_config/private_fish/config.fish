8990c432dd6d32282dc074dbb8d79ed721abbe92
Gadzhi Kharkharov <me@kkga.me>
2023-04-24 00:18:14 +0300

    Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,32 +1,4 @@
-set -gx OS (uname -s)
-set -gx GOPATH $HOME/.local/go
-set -gx DENO_INSTALL $HOME/.deno
-set -gx PNPM_HOME /Users/kkga/Library/pnpm
-set -gx BUN_INSTALL "$HOME/.bun"
-set -gx PNPM_HOME /Users/kkga/Library/pnpm
-set -gx BAT_THEME ansi
-set -gx FZF_DEFAULT_COMMAND 'fd --type=file'
-set -gx FZF_DEFAULT_OPTS '--layout=reverse --height=50% --preview-window=bottom:50%,border-top --inline-info --color=prompt:3,header:7,info:7,pointer:14:bold,marker:4,hl:4,hl+:12:,fg+:15,bg+:235'
-set -gx EDITOR hx
-set -gx VISUAL $EDITOR
-set -gx TDX_PATH $HOME/.local/share/calendars/mailbox
-set -gx TDX_LIST_OPTS '-g tag'
-set -gx TDX_ADD_OPTS '-l MzM'
-set -gx KKS_DEFAULT_SESSION default
-set -gx KKS_USE_GITDIR_SESSIONS 1
-set -gx ZK_NOTEBOOK_DIR $HOME/notes
-set -xg BAT_THEME ansi
-# set -gx DFT_DISPLAY inline
-
-if [ $OS = Linux ]
-    set -gx XBPS_DISTDIR $HOME/.void-packages
-    set -gx WEBKIT_USE_SINGLE_WEB_PROCESS 1
-    set -gx TERMINAL term
-    set -gx TERMCMD term
-    set -gx DMENU 'fuzzel --dmenu'
-    set -gx BROWSER browser
-    set -gx SSH_AUTH_SOCK "/run/user/$(id -u)/gcr/ssh"
-else if [ $OS = Darwin ]
+if [ $OS = Darwin ]
     fish_add_path /opt/homebrew/bin
     fish_add_path /opt/homebrew/sbin
     fish_add_path /opt/homebrew/opt/icu4c/bin
@@ -35,11 +7,6 @@ else if [ $OS = Darwin ]
     fish_add_path /usr/local/opt/openssl@1.1/bin
     fish_add_path /opt/homebrew/opt/tcl-tk/bin
     fish_add_path /opt/homebrew/opt/openjdk/bin
-    set -gx LDFLAGS -L/opt/homebrew/opt/unixodbc/lib
-    set -gx CPPFLAGS -I/opt/homebrew/opt/unixodbc/include
-    set -gx CPPFLAGS -I/opt/homebrew/opt/openjdk/include
-    set -gx KAKOUNE_POSIX_SHELL /opt/homebrew/bin/dash
-    set -gx JAVA_HOME (/usr/libexec/java_home)
 end
 
 fish_add_path \
