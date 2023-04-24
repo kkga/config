e28bf78f2d6a4430e008f9d274c84b87aeae1797
Gadzhi Kharkharov <me@kkga.me>
2023-04-25 01:07:09 +0300

    Update .config/fish/vars.fish

--- private_dot_config/fish/vars.fish
+++ private_dot_config/fish/vars.fish
@@ -2,20 +2,14 @@ set -gx OS (uname -s)
 set -gx GOPATH $HOME/.local/go
 set -gx DENO_INSTALL $HOME/.deno
 set -gx PNPM_HOME /Users/kkga/Library/pnpm
-set -gx BUN_INSTALL "$HOME/.bun"
-set -gx PNPM_HOME /Users/kkga/Library/pnpm
-set -gx BAT_THEME ansi
 set -gx FZF_DEFAULT_COMMAND 'fd --type=file'
 set -gx FZF_DEFAULT_OPTS '--layout=reverse --height=50% --preview-window=bottom:50%,border-top --inline-info --color=prompt:3,header:7,info:7,pointer:14:bold,marker:4,hl:4,hl+:12:,fg+:15,bg+:235'
 set -gx EDITOR hx
 set -gx VISUAL $EDITOR
-set -gx TDX_PATH $HOME/.local/share/calendars/mailbox
-set -gx TDX_LIST_OPTS '-g tag'
-set -gx TDX_ADD_OPTS '-l MzM'
-set -gx KKS_DEFAULT_SESSION default
-set -gx KKS_USE_GITDIR_SESSIONS 1
 set -gx ZK_NOTEBOOK_DIR $HOME/notes
-set -xg BAT_THEME ansi
+
+set -gx BAT_THEME ansi
+set -gx BAT_STYLE plain
 
 if [ $OS = Linux ]
     set -gx SSH_AUTH_SOCK "/run/user/$(id -u)/gcr/ssh"
