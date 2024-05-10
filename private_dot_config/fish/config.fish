9b821513908342eda975f7a6d2ea319529dd93dd
Gadzhi Kharkharov <me@kkga.me>
2024-05-10 10:49:49 +0300

    Update .config/fish/config.fish Update .config/yabai/yabairc

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -7,7 +7,7 @@ set -gx PNPM_HOME /Users/kkga/Library/pnpm
 set -gx FZF_DEFAULT_COMMAND 'fd --type=file'
 set -gx FZF_DEFAULT_OPTS '--layout=reverse --height=50% --preview-window=bottom:50%,border-top --inline-info --color=prompt:3,header:7,info:7,pointer:14:bold,marker:4,hl:4,hl+:12:,fg+:15,bg+:235'
 set -gx EDITOR hx
-set -gx VISUAL $EDITOR
+set -gx VISUAL code
 set -gx ZK_NOTEBOOK_DIR $HOME/notes
 set -gx BAT_THEME ansi
 set -gx BAT_STYLE plain
